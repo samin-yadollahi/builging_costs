2b4aa440b985b68241eeddfef73900bb6c9c956f
samin-yadollahi <yadollahisamin4@gmail.com>
2023-06-13 15:52:13 +0330

    refactor the code, fix the parent class and get the sel.price in each child class

--- cost.py
+++ cost.py
@@ -7,9 +7,6 @@ from abc import ABC, abstractmethod
 
 class Cost(ABC):
 
-    def __init__(self, price):
-        self.price = price  #remove and get in the child class
-
     @abstractmethod
     def show_the_object(self):
         pass
@@ -17,27 +14,35 @@ class Cost(ABC):
 
 class Complex(Cost):
 
-    def show_the_object(self):
+    def __init__(self, price):
+        self.price = price
 
+    def show_the_object(self):
         return(f"the Complex cost is, {self.price}")
 
 
 class Block(Cost):
 
-    def show_the_object(self):
+    def __init__(self, price):
+        self.price = price
 
+    def show_the_object(self):
         return(f"the Block cost is, {self.price}")
 
 
 class Floor(Cost):
 
-    def show_the_object(self):
+    def __init__(self, price):
+        self.price = price
 
+    def show_the_object(self):
         return(f"the Floor cost is, {self.price}")
 
 
 class Unit(Cost):
 
-    def show_the_object(self):
+    def __init__(self, price):
+        self.price = price
 
+    def show_the_object(self):
         return(f"the Unit cost is, {self.price}")
